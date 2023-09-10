9e577eea3d007f88a677087043d59be8c1b81317
suo235 <usukeo235@gmail.com>
2023-09-10 22:22:40 +0900

    Add select_random_k() and select_best_k()

--- include/gapp/gapp.hpp
+++ include/gapp/gapp.hpp
@@ -85,6 +85,73 @@ std::vector<individual::Individual<GeneType, FitnessType>> tournament(const std:
 
     return ret;
 }
+
+template<typename GeneType, typename FitnessType, typename RandomEngine>
+std::vector<individual::Individual<GeneType, FitnessType>> select_random_k(const std::vector<individual::Individual<GeneType, FitnessType>>& population, const std::size_t select_size, RandomEngine&& engine)
+{
+    std::vector<individual::Individual<GeneType, FitnessType>> ret(select_size);
+
+    std::vector<std::size_t> temp_indices(population.size());
+    for(std::size_t i = 0; i < temp_indices.size(); i ++)
+    {
+        temp_indices[i] = i;
+    }
+
+    // Fisher-Yates
+    for(std::size_t i = 0; i < select_size; i ++)
+    {
+        std::uniform_int_distribution<std::size_t> distribution(i, population.size()-1);
+        std::size_t index = distribution(engine);
+        
+        ret[i].gene = population[temp_indices[index]].gene;
+        ret[i].fitness = population[temp_indices[index]].fitness;
+
+        std::swap(temp_indices[i], temp_indices[index]);
+    }
+
+    return ret;
+}
+
+template<typename GeneType, typename FitnessType>
+std::vector<individual::Individual<GeneType, FitnessType>> select_best_k(const std::vector<individual::Individual<GeneType, FitnessType>>& population, const std::size_t select_size, const ProblemType problem_type)
+{
+    using Individual = individual::Individual<GeneType, FitnessType>;
+
+    std::vector<Individual> ret(select_size);
+
+    std::vector<Individual*> pointers(population.size());
+
+    // ポインタ配列を作成
+    for(std::size_t i = 0; i < pointers.size(); i ++)
+    {
+        pointers[i] = const_cast<Individual*>(population.data()) + i;
+    }
+
+    if(problem_type == ProblemType::MAXIMIZE)
+    {
+        // fitnessが大きい順に上位select_size個のポインタを取得
+        std::nth_element(pointers.begin(), pointers.begin()+select_size, pointers.end(),
+            [](const Individual* a, const Individual* b)
+            {
+                return a->fitness > b->fitness;
+            });
+    }
+    else
+    {
+        std::nth_element(pointers.begin(), pointers.begin()+select_size, pointers.end(),
+            [](const Individual* a, const Individual* b)
+            {
+                return a->fitness < b->fitness;
+            });
+    }
+
+    for(std::size_t i = 0; i < select_size; i ++)
+    {
+        ret[i] = *(pointers[i]);
+    }
+
+    return ret;
+}
 }
 
 }
