9ca1ac8d81fa8e5125262c09706518d2c71b4acf
suo235 <usukeo235@gmail.com>
2023-09-10 22:26:59 +0900

    Add basic crossover algorithms: k_points(), blx_alpha()

--- include/gapp/gapp.hpp
+++ include/gapp/gapp.hpp
@@ -154,6 +154,77 @@ std::vector<individual::Individual<GeneType, FitnessType>> select_best_k(const s
 }
 }
 
+namespace crossover
+{
+template<typename GeneType, typename FitnessType, typename Engine>
+std::vector<individual::Individual<GeneType, FitnessType>> k_points(const individual::Individual<GeneType, FitnessType>& a, const individual::Individual<GeneType, FitnessType>& b, const std::size_t k_points, Engine&& engine)
+{
+    std::vector<individual::Individual<GeneType, FitnessType>> ret{a, b};
+
+    std::vector<std::size_t> indices(a.gene.size());
+    for(std::size_t i = 0; i < indices.size(); i ++)
+    {
+        indices[i] = i;
+    }
+
+    // ランダムにk個のインデックスを選ぶ
+    // Fisher-Yates
+    for(std::size_t i = 0; i < k_points; i ++)
+    {
+        std::uniform_int_distribution<std::size_t> distribution(i, a.gene.size()-1);
+        std::size_t index = distribution(engine);
+        std::swap(indices[i], indices[index]);
+    }
+
+    if(k_points == 1)
+    {
+        std::swap_ranges(ret[0].gene.begin(), ret[0].gene.begin()+indices[0], ret[1].gene.begin());
+        return ret;
+    }
+
+    // k個(k>=2)のインデックスを昇順ソート
+    std::sort(indices.begin(), indices.begin()+k_points);
+
+    // k_points=2: i=0
+    // k_points=3: i=0
+    // k_points=4: i=0, i=2
+    for(std::size_t i = 0; i < k_points-1; i = i+2)
+    {
+        std::swap_ranges(ret[0].gene.begin()+indices[i], ret[0].gene.begin()+indices[i+1], ret[1].gene.begin()+indices[i]);
+    }
+
+    if((k_points%2) != 0)
+    {
+        std::swap_ranges(ret[0].gene.begin()+indices.back(), ret[0].gene.end(), ret[1].gene.begin()+indices.back());
+    }
+
+    return ret;
+}
+
+template<typename GeneType, typename FitnessType, typename Engine>
+std::vector<individual::Individual<GeneType, FitnessType>> blx_alpha(const individual::Individual<GeneType, FitnessType>& a, const individual::Individual<GeneType, FitnessType>& b, const double alpha, Engine&& engine)
+{
+    std::vector<individual::Individual<GeneType, FitnessType>> ret{a, b};
+
+    std::uniform_real_distribution<> generator(0.0, 1.0);
+
+    for(std::size_t i = 0; i < a.gene.size(); i ++)
+    {
+        auto x_min = std::min(a.gene[i], b.gene[i]);
+        auto x_max = std::max(a.gene[i], b.gene[i]);
+
+        auto d = std::abs(a.gene[i] - b.gene[i]);
+
+        auto r_min = x_min - alpha * d;
+        auto r_max = x_max + alpha * d;
+
+        ret[0].gene[i] = (r_max - r_min) * generator(engine) + r_min;
+        ret[1].gene[i] = (r_max - r_min) * generator(engine) + r_min;
+    }
+
+    return ret;
+}
+}
 }
 
 #endif /* GA_PLUS_HPP */
